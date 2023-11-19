0ba0ef0b5f0b0b3dccf8ce8d8a68e12b9e116cf5
LackErik <93646851+LackErik@users.noreply.github.com>
2023-11-19 14:45:23 +0100

    sound files

--- Assets/Scripts/Game1.cs
+++ Assets/Scripts/Game1.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Game1 : MonoBehaviour
 {
-    [SerializeField] private Timer _timer;
     [SerializeField] private PlayerProgress _playerProgress;
     public float rotationSpeed;
     public Transform rotationPoint;
     public InputManager _inputManager;
+    public bool standUpStatus; 
 
 
     void Update()
@@ -16,18 +16,31 @@ public class Game1 : MonoBehaviour
         if (_playerProgress.timeState != TimeState.Minigame) return;
         // Berechne die Achse, um die gedreht werden soll (hier Z-Achse)
         Vector3 axis = new Vector3(0, 0, 1);
-
-        if (Mathf.Abs(transform.rotation.z) >= 0.7)
+        if(standUpStatus)
         {
-            _timer.successful = true;
-            return;
-        } 
-        
-        if (_inputManager.buttonsPressed[3])
-        {
-            // Rotiere das GameObject um den angegebenen Punkt und Achse basierend auf der Zeit und der Rotationssgeschwindigkeit
-            transform.RotateAround(rotationPoint.transform.position, axis, rotationSpeed );
+            if (_inputManager.buttonsPressed[3])
+            {
+                // Rotiere das GameObject um den angegebenen Punkt und Achse basierend auf der Zeit und der Rotationssgeschwindigkeit
+                if (Mathf.Abs(transform.rotation.z) < 0.7)
+                {
+                    Debug.Log(transform.rotation.z);
+                    transform.RotateAround(rotationPoint.transform.position, axis, rotationSpeed);
+                }
+            }
         }
+        else
+        {
+            if (_inputManager.buttonsPressed[3])
+            {
+                // Rotiere das GameObject um den angegebenen Punkt und Achse basierend auf der Zeit und der Rotationssgeschwindigkeit
+                if (transform.rotation.z < 0)
+                {
+                   
+                    transform.RotateAround(rotationPoint.transform.position, axis, rotationSpeed);
+                }
+            }
+        }
+            
         
     }
 }
